8d6fbf3a05adc83cc4caf8616b935be378ea927e
D4ITON <daitonmh100@gmail.com>
2018-11-01 18:51:24 -0500

    primer push generacion de aleatorios

---
+++ index.view.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>RandomApp</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body>
+	<section class="grid-container">
+		<div class="grid-item generacion">
+			GENERACIÓN DE NÚMEROS ALEATORIOS
+		</div>
+		<div class="grid-item algoritmo">
+			<h3>ALGORITMO</h3>
+			<br>
+			<div class="metodos">
+				<a href="#">Cuadrados Medios</a>
+				<a href="#">Productos Medios</a>
+				<a href="#">Multiplicador Constante</a>
+				<a href="#">Algoritmo Lineal</a>
+				<a href="#">Multiplicativo</a>
+				<a href="#">Aditivo</a>
+				<a href="#">Cuadrático</a>
+				<a href="#">BlumBlum&Shub</a>
+			</div>
+		</div>
+		<div class="grid-item ingreso">
+			INGRESO DE DATOS
+			<form action="">
+				
+			</form>
+		</div>
+		<div class="grid-item descarga">
+			<a href="reportes/file01.txt" download="aleatorios">
+				Descargar Resultados
+			</a>
+		</div>
+	</section>
+	<footer>
+		
+	</footer>
+</body>
+</html>
